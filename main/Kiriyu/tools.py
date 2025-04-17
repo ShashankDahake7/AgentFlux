85a1de7c83ad8fff23b6f1265d9246f91c376a61
VidyutChakrabarti <chakrabartivr@rknec.edu>
2025-04-17 22:16:21 +0530

    adding codedocs search tool

--- main/Kiriyu/tools.py
+++ main/Kiriyu/tools.py
@@ -1,11 +1,6 @@
-from langchain.tools import tool 
-from datetime import date
+from crewai_tools import CodeDocsSearchTool
 
+codedoc_tool = CodeDocsSearchTool(docs_url='https://langchain-ai.github.io/langgraph/tutorials/introduction/')
 
-@tool("Get current date")
-def getCurrentDate():
-    """used to fetch the current date in a formatted manner."""
-    today = date.today()
-    d = today.strftime("%d/%m/%Y")
-    return d
-
+#this tool will be directed towards a custom streamlit page containing each and every code snippet needed to integrate models with our code. also custom tool integration code. 
+#for agent_architect.
